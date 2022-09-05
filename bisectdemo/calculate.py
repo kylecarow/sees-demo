31dbf22e0c541655ec472d4adc5d61d61e21265d
Kyle Carow <kcarow@nrel.gov>
2022-09-05 16:41:11 -0400

    introduce subtract functionality and test

--- bisectdemo/calculate.py
+++ bisectdemo/calculate.py
@@ -2,6 +2,10 @@ def add(a: float, b: float) -> float:
     """Returns the result of `a` added with `b`"""
     return a + b
 
+def subtract(a: float, b: float) -> float:
+    """Returns the result of `b` subtracted from `a`"""
+    return a - b
+
 def multiply(a: float, b: float) -> float:
     """Returns the result of `a` multiplied with `b`"""
-    return a * b
+    return a * b * b
--- tests/test_calculate.py
+++ tests/test_calculate.py
@@ -1,4 +1,4 @@
-from bisectdemo.calculate import add, multiply
+from bisectdemo.calculate import add, subtract, multiply
 
 def test_add():
     assert add(1.0, 1.0) == 2.0
@@ -7,6 +7,14 @@ def test_add():
     assert add(4.0, 2.0) == 6.0
     assert add(5.0, 3.0) == 8.0
     assert add(6.0, 3.0) == 9.0
+
+def test_subtract():
+    assert subtract(1.0, 1.0) == 0.0
+    assert subtract(2.0, 1.0) == 1.0
+    assert subtract(3.0, 2.0) == 1.0
+    assert subtract(4.0, 2.0) == 2.0
+    assert subtract(5.0, 3.0) == 2.0
+    assert subtract(6.0, 3.0) == 3.0
 
 def test_multiply():
     assert multiply(1.0, 1.0) == 1.0
