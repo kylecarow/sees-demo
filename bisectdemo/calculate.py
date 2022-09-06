511dae74c6e88d963f56a5561b40e766df0f6356
Kyle Carow <kcarow@nrel.gov>
2022-09-06 11:51:12 -0400

    fix bug in multiply

--- bisectdemo/calculate.py
+++ bisectdemo/calculate.py
@@ -8,7 +8,7 @@ def subtract(a: float, b: float) -> float:
 
 def multiply(a: float, b: float) -> float:
     """Returns the result of `a` multiplied with `b`"""
-    return a * b * b
+    return a * b
 
 def divide(a: float, b: float) -> float:
     """Returns the result of `a` divided by `b`"""
